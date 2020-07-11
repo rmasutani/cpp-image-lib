7d93df50378da6b64377a534a33983f1766bc283
rmasutani <masutanirugby@gmail.com>
2020-07-11 17:40:11 +0900

    Added library configs

--- ImageCopy/main.c
+++ ImageCopy/main.c
@@ -1,49 +1,49 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-    FILE *streamIn = fopen("images/cameraman.bmp", "rb");
-    FILE *fout = fopen("images/cameraman_copy.bmp", "wb");
+int main()
+{
+    FILE *streamIn = fopen("images/cameraman.bmp","rb");
+    FILE *fo = fopen("images/cameraman_copy.bmp","wb");
 
-    if (streamIn == (FILE*)0) {
+    if(streamIn ==(FILE*)0)
+    {
         printf("Unable to open file\n");
     }
-
     unsigned char header[54];
     unsigned char colorTable[1024];
 
-    for (int i=0; i < 54; i++) {
-        header[i] = getc(streamIn);
+    for(int i =0;i<54;i++)
+    {
+        header[i] =getc(streamIn);
+
     }
 
-    // printf("header[10]: %d\n", header[10]);
-    // printf("(int)header[10]: %d\n", (int)header[10]);
-    // printf("(void *)&header[10]: %s\n", (void *)&header[10]);
-    // printf("&header[10]: %p\n", &header[10]);
-    // printf("*(int *)&header[10]: %d\n", *(int *)&header[10]);
+    int width = *(int *)&header[18];
+    int height = *(int *)&header[22];
+    int bitDepth = *(int *)&header[28];
 
-
-    int width = *(int *) &header[10];
-    int height = *(int *) &header[22];
-    int bitDepth = *(int *) &header[28];
-
-    if (bitDepth <= 8) {
-        fread(colorTable, sizeof(unsigned char), 1024, streamIn);
+    if(bitDepth<=8)
+    {
+         fread(colorTable,sizeof(unsigned char), 1024,streamIn);
     }
 
-    fwrite(header, sizeof(unsigned char), 54, fout);
-    unsigned char buf[height*width];
-    fread(buf, sizeof(unsigned char), (height*width), streamIn);
+    fwrite(header,sizeof(unsigned char),54,fo);
+    unsigned char buf[height * width];
+   fread(buf,sizeof(unsigned char),(height*width), streamIn);
 
-    if (bitDepth <= 8) {
-        fwrite(colorTable, sizeof(unsigned char), 1024, fout);
-    }
-    fwrite(buf, sizeof(unsigned char), (height*width), fout);
-    fclose(fout);
-    fclose(streamIn);
+   if(bitDepth <=8)
+   {
 
-    printf("Sussess!\n");
-    printf("Width: %d, Height: %d", width, height);
+       fwrite(colorTable,sizeof(unsigned char),1024,fo);
+   }
+       fwrite(buf,sizeof(unsigned char),(height *width),fo);
+       fclose(fo);
+       fclose(streamIn);
+
+       printf("Success !\n");
+       printf("Width : %d\n", width);
+       printf("Height : %d\n",height);
 
     return 0;
 }
